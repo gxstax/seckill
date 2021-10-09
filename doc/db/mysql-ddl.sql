68bc14dca02b41a11d7eae3d97de0d8d00ae6f6a
马以 <gxstaxant@gmail.com>
2021-10-09 18:30:28 +0800

    秒杀数据表提交

---
+++ doc/db/mysql-ddl.sql
@@ -0,0 +1,55 @@
+-- 商品表
+CREATE TABLE `goods` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `no` varchar(45) NOT NULL COMMENT '商品编号',
+  `name` varchar(45) NOT NULL COMMENT '商品名称',
+  `picture_url` varchar(64) NULL COMMENT '图片',
+  `normal_price` int(10) NOT NULL DEFAULT 1  COMMENT '正常价格',
+  `mark` int(10) NOT NULL DEFAULT 0 COMMENT  '商品标识',
+  `state` tinyint(1) DEFAULT 0 NOT NULL COMMENT '账户状态（0:正常 1:锁定）',
+  `version` int(10) DEFAULT 0 NOT NULL COMMENT '版本号',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_no` (`no`),
+  KEY `idx_created_at` (`created_at`)
+ ) ENGINE=InnoDB COMMENT = '商品表';
+
+ -- 秒杀活动表
+ CREATE TABLE `activity` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `no` varchar(45) NOT NULL COMMENT '活动编号',
+  `name` varchar(45) NOT NULL COMMENT '活动名称',
+  `goods_no` varchar(45) NOT NULL COMMENT '活动商品编号',
+  `stock` int(10) unsigned NOT NULL  DEFAULT 0 COMMENT '活动库存',
+  `start_time` timestamp NULL DEFAULT NULL COMMENT '活动开始时间',
+  `end_time` timestamp NULL DEFAULT NULL COMMENT '活动结束时间',
+  `state` tinyint(1) DEFAULT 0 NOT NULL COMMENT '账户状态（0:正常 1:结束）',
+  `version` int(10) DEFAULT 0 NOT NULL COMMENT '版本号',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_no` (`no`),
+  KEY `idx_goods_no` (`goods_no`),
+  KEY `idx_created_at` (`created_at`)
+ ) ENGINE=InnoDB COMMENT = '秒杀活动表';
+
+-- 秒杀订单表
+ CREATE TABLE `orders` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `no` varchar(45) NOT NULL COMMENT '订单编号',
+  `user_no` bigint(20) unsigned NOT NULL COMMENT '用户编号',
+  `goods_no` varchar(45) NOT NULL COMMENT '活动商品编号',
+  `state` tinyint(1) DEFAULT 0 NOT NULL COMMENT '状态（0:正常 1:关闭）',
+  `version` int(10) DEFAULT 0 NOT NULL COMMENT '版本号',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_no` (`no`),
+  KEY `idx_created_at` (`created_at`)
+ ) ENGINE=InnoDB COMMENT = '秒杀订单表';
+
+
--- server-nginx/domain/domain.com
+++ server-nginx/domain/domain.com
@@ -3,9 +3,41 @@ server {
     listen 7081;
     location /sayhello {
         default_type text/plain;
-            proxy_pass http://backend/demo;
+        proxy_pass http://backend/demo;
         #content_by_lua_block {
         #    ngx.say("hello world!!!")
         #}
     }
+
+    #活动数据查询
+    location /activity/query {
+            default_type text/plain;
+            proxy_pass http://backend/demo;
+    }
+
+    # 进结算页页面（H5）
+    location /settlement/page {
+            default_type text/html;
+            proxy_pass http://backend/demo;
+    }
+
+    # 结算页页面初始化渲染所需数据
+    location /settlement/initData {
+            default_type text/plain;
+            proxy_pass http://backend/demo;
+    }
+
+    # 结算页提交订单
+    location /settlement/submitData {
+            default_type text/plain;
+            proxy_pass http://backend/demo;
+
+    }
+
+    # 结算页用户行为操作，模糊匹配
+    location ～*/settlement/submitData {
+            default_type text/plain;
+            proxy_pass http://backend/demo;
+    }
+
 }
